f221ab6bcdc8195963d6762e67b272da3395ef71
Adi Rifta Dwi Kurniawan <adiriftadk@gmail.com>
2026-01-05 12:20:55 +0700

    fixxx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,7 +4,7 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
-import ThemeProvider from './components/Dark/ThemeProvider';
+import ThemeProvider from './components/dark-test/ThemeProvider';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
